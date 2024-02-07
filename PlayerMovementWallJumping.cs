61b593415aca2aaa9349a9088567da8c3cdf4f86
iliaswimrun <104718725+iliaswimrun@users.noreply.github.com>
2024-02-07 15:17:20 +0200

    Update and rename PlayerMovement.cs to PlayerMovementWallJumping.cs

--- PlayerMovementWallJumping.cs
+++ PlayerMovementWallJumping.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementWallJumping : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
