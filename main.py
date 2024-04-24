af58f6cedc66a575e05cb9e8052e98d3d89b7cfe
iliaswimrun <104718725+iliaswimrun@users.noreply.github.com>
2024-04-24 15:59:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def remove_bg():
 
     for ext in list_of_extensions:
         all_files.extend(
-            Path('C:\\Users\\Professional\\PythonImg\\input_imgs').glob(ext))
+            Path('шлях до папки із вхідними зображеннями').glob(ext))
 
     for index, item in enumerate(all_files):
         input_path = Path(item)
         file_name = input_path.stem
 
-        output_path = f'C:/Users/Professional/PythonImg/output_imgs/{file_name}_output.png'
+        output_path = f'шлях до папки з вихідними зображеннями/{file_name}_output.png'
 
         input_img = Image.open(input_path)
         output_img = remove(input_img)
